1366b39a64221f5aacde916fec6665fc07d4be9e
Fatima <fatimabha99@gmail.com>
2022-02-02 13:13:56 +0100

    empezando proyecto

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿ using System;
+
+namespace proyecto_consola
+{
+    class Program
+    {
+        static void Main(string[] args) // método principal
+        {
+           string mensaje2 ="";
+           Console.WriteLine("Introduzca un texto");
+           mensaje2= Console.ReadLine();
+           string mensajedesalida ="";
+           Console.WriteLine("Introduzca un texto");
+           mensajedesalida = Console.ReadLine();
+           
+           bool resultado = mensaje2.Equals(mensajedesalida);
+           Console.WriteLine(resultado);
+           Console.ReadLine();
+           
+           
+         
+           
+           
+        
+       }
+
+    }
+}
